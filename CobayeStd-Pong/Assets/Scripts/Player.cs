3b53adb48c6a3c0400eb756cb0b282d88accf8c9
Veon7 <alexandreflores@hotmail.fr>
2020-04-11 16:44:06 +0200

    F4 player class

---
+++ CobayeStd-Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static float speed = 1f;
+
+    public KeyCode upKey = KeyCode.UpArrow;
+    public KeyCode downKey = KeyCode.DownArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(downKey))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+}
