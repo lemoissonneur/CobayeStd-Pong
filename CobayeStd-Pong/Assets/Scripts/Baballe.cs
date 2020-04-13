6a1502c6110984ce505b0ef2445b476b2da0f8bb
Veon7 <alexandreflores@hotmail.fr>
2020-04-13 18:26:39 +0200

    F006: baballe
    Ajout en plus d'une limite de vitesse

--- CobayeStd-Pong/Assets/Scripts/Baballe.cs
+++ CobayeStd-Pong/Assets/Scripts/Baballe.cs
@@ -7,6 +7,8 @@ public class Baballe : MonoBehaviour
 {
     public float initialSpeed = 1f;
     public int start_delay_sec = 3;
+    public float reboundAcceleration = 0.5f;
+    public float limitSpeed = 10f;
 
     AudioSource audioSource;
     public AudioClip pingClip;
@@ -14,11 +16,14 @@ public class Baballe : MonoBehaviour
     public AudioClip wallClip;
 
     private bool started = false;
+    private float currentSpeed;
+    private Rigidbody2D rb2D;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,7 +32,10 @@ public class Baballe : MonoBehaviour
         if(!started && Time.time > start_delay_sec)
         {
             started = true;
-            // lancement de la balle ici
+
+            // Lancement de la balle
+            rb2D.velocity = Vector2.right * initialSpeed;
+            currentSpeed = initialSpeed;
         }
         
     }
@@ -37,23 +45,51 @@ public class Baballe : MonoBehaviour
         switch (other.gameObject.name)
         {
             case "Ping":
-                Debug.Log("ping");
-                audioSource.PlayOneShot(pingClip, 1f);
+                {
+                    Debug.Log("ping");
+                    audioSource.PlayOneShot(pingClip, 1f);
+
+                    float y = HitFactor(transform.position, other.transform.position, other.bounds.size.y);
+                    Vector2 dir = new Vector2(1, y).normalized;
+                    currentSpeed = (currentSpeed + reboundAcceleration) > limitSpeed ? limitSpeed : (currentSpeed + reboundAcceleration);
+                    rb2D.velocity = dir * currentSpeed;
+                }
                 break;
 
             case "Pong":
-                Debug.Log("pong");
-                audioSource.PlayOneShot(pongClip, 1f);
+                {
+                    Debug.Log("pong");
+                    audioSource.PlayOneShot(pongClip, 1f);
+
+                    float y = HitFactor(transform.position, other.transform.position, other.bounds.size.y);
+                    Vector2 dir = new Vector2(-1, y).normalized;
+                    currentSpeed = (currentSpeed + reboundAcceleration) > limitSpeed ? limitSpeed : (currentSpeed + reboundAcceleration);
+                    rb2D.velocity = dir * currentSpeed;
+                }
                 break;
 
             case "TopWall":
-                Debug.Log("top");
-                audioSource.PlayOneShot(wallClip, 1f);
+                {
+                    Debug.Log("top");
+                    audioSource.PlayOneShot(wallClip, 1f);
+
+                    Vector2 inDirection = rb2D.velocity;
+                    Vector2 inNormal = Vector2.down;
+                    Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal);
+                    rb2D.velocity = newVelocity;
+                }
                 break;
 
             case "BottomWall":
-                Debug.Log("bottom");
-                audioSource.PlayOneShot(wallClip, 1f);
+                {
+                    Debug.Log("bottom");
+                    audioSource.PlayOneShot(wallClip, 1f);
+
+                    Vector2 inDirection = rb2D.velocity;
+                    Vector2 inNormal = Vector2.up;
+                    Vector2 newVelocity = Vector2.Reflect(inDirection, inNormal);
+                    rb2D.velocity = newVelocity;
+                }
                 break;
 
             case "LeftWall":
@@ -62,4 +98,11 @@ public class Baballe : MonoBehaviour
                 break;
         }
     }
+
+
+    private float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+
 }
