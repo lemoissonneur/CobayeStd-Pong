bb5c95ec1d266f4dc316bc5d35abcf4f26bf54a9
lemoissonneur <lemoissonneur@users.noreply.github.com>
2020-04-15 19:31:24 +0200

    F102: map 4j, edit prefab player

--- CobayeStd-Pong/Assets/Scripts/Player.cs
+++ CobayeStd-Pong/Assets/Scripts/Player.cs
@@ -5,51 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 3f;
-    public enum deplacementMode {HORIZONTAL, VERTICAL};
-    public deplacementMode deplacement = deplacementMode.HORIZONTAL;
-    public KeyCode upKey = KeyCode.None;
-    public KeyCode downKey = KeyCode.None;
+    public KeyCode upRightKey = KeyCode.None;
+    public KeyCode downLeftKey = KeyCode.None;
 
-    public float verticalLimitePos = 4.2f;
-    public float horizontalLimitePos = 8.1f;
-    public float limite = 0f;
+    public float LimitePos = 4.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (deplacement == deplacementMode.HORIZONTAL)
-            deplacementHorizontal();
-        else if (deplacement == deplacementMode.VERTICAL)
-            deplacementVertical();
-    }
-
-    void deplacementVertical()
-    {
-        if (Input.GetKey(upKey) && transform.position.y < verticalLimitePos)
+        if (Input.GetKey(upRightKey) && transform.position.y < LimitePos)
         {
             transform.Translate(Vector2.up * speed * Time.deltaTime);
         }
-        if (Input.GetKey(downKey) && transform.position.y > -verticalLimitePos)
+        if (Input.GetKey(downLeftKey) && transform.position.y > -LimitePos)
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
-        }
-    }
-
-    void deplacementHorizontal()
-    {
-        if (Input.GetKey(RightKey) && transform.position.x < horizontalLimitePos)
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(LeftKey) && transform.position.x > -horizontalLimitePos)
-        {
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
     }
 }
