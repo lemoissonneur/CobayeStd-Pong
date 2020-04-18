d205977b6f8c8bb5af2af59db430dfb2442c90cb
lemoissonneur <lemoissonneur@users.noreply.github.com>
2020-04-18 18:09:30 +0200

    F113: fusion des deux IA en une seul class avec paramètre 'niveau'

--- CobayeStd-Pong/Assets/Scripts/IAplayer.cs
+++ CobayeStd-Pong/Assets/Scripts/IAplayer.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IApete : MonoBehaviour
+public class IAplayer : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 6.5f;
     public float LimitePos = 4.2f;
+    public enum IANiveau {STUPIDE, PETE};
+    public IANiveau niveau;
     private GameObject balle;
     private Vector3 ballePosition;
     private Vector3 ballePreviousPosition;
     private Vector3 balleDirection;
-    public float offset;
+    private float offset;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,25 @@ public class IApete : MonoBehaviour
         balle = GameObject.Find("Baballe");
         ballePosition = balle.GetComponent<Transform>().position;
         ballePreviousPosition = ballePosition;
-        offset = (this.GetComponent<BoxCollider2D>().size.y /2) -0.1f;
+        offset = (this.GetComponent<BoxCollider2D>().size.y / 2) - 0.1f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        switch(niveau)
+        {
+            case IANiveau.STUPIDE:
+                IAstupideUpdate();
+                break;
+
+            case IANiveau.PETE:
+                IApeteUpdate();
+                break;
+        }
+    }
+
+    private void IApeteUpdate()
     {
         // on récup la position actuelle de la balle
         ballePosition = balle.GetComponent<Transform>().position;
@@ -43,6 +59,19 @@ public class IApete : MonoBehaviour
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
         }
+    }
 
+    private void IAstupideUpdate()
+    {
+        ballePosition = balle.GetComponent<Transform>().position;
+
+        if (ballePosition.y > transform.position.y && transform.position.y < LimitePos)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        if (ballePosition.y < transform.position.y && transform.position.y > -LimitePos)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
     }
 }
